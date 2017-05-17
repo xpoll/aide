7e470f1986d05d14f6d8fba44e47d4397f4fb592
xpoll <blmdz521@126.com>
2017-05-17 21:42:55 +0800

    aide init

---
+++ qxx-common/src/main/java/cn/blmdz/common/model/BaseUser.java
@@ -0,0 +1,14 @@
+package cn.blmdz.common.model;
+
+import java.io.Serializable;
+
+public interface BaseUser extends Serializable {
+
+    Long getId();
+
+    String getName();
+
+//    String getType();
+
+//    List<String> getRoles();
+}
